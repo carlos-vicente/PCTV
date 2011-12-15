382cad70da85983f751154c6576aea9dcb8ad446
Carlos Vicente <carlosvicente200@hotmail.com>
2011-12-15 22:40:50 +0000

    Added service host

--- PCTV/PCTV.ExternalInput.Host/Program.cs
+++ PCTV/PCTV.ExternalInput.Host/Program.cs
@@ -4,42 +4,62 @@ using Microsoft.ApplicationServer.Http;
 using Ninject;
 using PCTV.ExternalInput.Service;
 using Microsoft.ApplicationServer.Http.Description;
+using Ninject.Parameters;
 
 namespace PCTV.ExternalInput.Host
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            IKernel kernel = new StandardKernel(new PCTV.Input.DependencyModule(), new PCTV.ExternalInput.DependencyModule());
+        //static void Main(string[] args)
+        //{
+        //    IKernel kernel = new StandardKernel(new PCTV.Input.DependencyModule(), new PCTV.ExternalInput.DependencyModule());
 
-            IExternalInput service = kernel.Get<IExternalInput>();
+        //    IExternalInput service = kernel.Get<IExternalInput>();
 
-            HttpConfiguration conf = new HttpConfiguration();
-            conf.EnableTestClient = true;
-            conf.IncludeExceptionDetail = true;
+        //    HttpConfiguration conf = new HttpConfiguration();
+        //    conf.EnableTestClient = true;
+        //    conf.IncludeExceptionDetail = true;
 
-            using (var host = new HttpServiceHost(service, conf, new Uri("http://localhost:8080/ExternalInput")))
-            {
-                host.Open();
-                ShowEndpointsOf(host);
-                WaitForKey();
-            }
-        }
+        //    using (var host = new HttpServiceHost(service, conf, new Uri("http://localhost:8080/ExternalInput")))
+        //    {
+        //        host.Open();
+        //        ShowEndpointsOf(host);
+        //        WaitForKey();
+        //    }
+        //}
 
-        private static void ShowEndpointsOf(ServiceHost host)
-        {
-            Console.WriteLine("Host is opened with the following endpoints:");
-            foreach (var ep in host.Description.Endpoints)
-            {
-                Console.WriteLine("\tEndpoint: address = {0}; binding = {1}", ep.Address, ep.Binding);
-            }
-        }
+        //private static void ShowEndpointsOf(ServiceHost host)
+        //{
+        //    Console.WriteLine("Host is opened with the following endpoints:");
+        //    foreach (var ep in host.Description.Endpoints)
+        //    {
+        //        Console.WriteLine("\tEndpoint: address = {0}; binding = {1}", ep.Address, ep.Binding);
+        //    }
+        //}
 
         private static void WaitForKey()
         {
             Console.WriteLine("Press any key to stop host...");
             Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                IKernel kernel = new StandardKernel(new PCTV.Input.DependencyModule(), new PCTV.ExternalInput.DependencyModule());
+
+                using (var host = kernel.Get<Host>(new ConstructorArgument("baseUrl", "http://localhost:8080/ExternalInput")))
+                {
+                    host.Open();
+                    WaitForKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The following error occured: {0}", ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
         }
     }
 }
---
+++ PCTV/PCTV.ExternalInput/Host/Host.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.ApplicationServer.Http;
+using Ninject.Extensions.Logging;
+using PCTV.ExternalInput.Service;
+
+namespace PCTV.ExternalInput.Host
+{
+    public class Host : IDisposable
+    {
+        private ILogger _logger;
+        private IExternalInput _service;
+        private HttpServiceHost _host;
+        private String _baseUrl;
+
+        public Host(IExternalInput service, ILogger logger, String baseUrl)
+        {
+            _logger = logger;
+            _service = service;
+            _baseUrl = baseUrl;
+        }
+
+        public void Open()
+        {
+            HttpConfiguration conf = new HttpConfiguration();
+            conf.EnableTestClient = true;
+            conf.IncludeExceptionDetail = true;
+
+            _host = new HttpServiceHost(_service, conf, new Uri(_baseUrl));
+            _host.Open();
+
+            _logger.Info("Host is opened with the following endpoints:");
+            foreach (var ep in _host.Description.Endpoints)
+            {
+                _logger.Info("\tEndpoint: address = {0}; binding = {1}", ep.Address, ep.Binding);
+            }
+        }
+    
+        public void Dispose()
+        {
+            if (_host != null && _host.State == CommunicationState.Opened)
+                _host.Close();
+        }
+    }
+}
